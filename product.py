93767bd9c480ba437df1ae78c7216b28c101b565
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-08-26 00:42:18 +0200

    fix product.py

--- product.py
+++ product.py
@@ -1,84 +1,99 @@
-import unittest
-from product import *
+import os
+from flask import Flask, jsonify, request
+from requests_oauthlib import OAuth1Session
+import redis
+from dotenv import load_dotenv
 
-class TestProduct(unittest.TestCase):
-    def setUp(self):
-        app.testing = True
-        self.app = app.test_client()
+load_dotenv() 
 
-    def test_get_product_success(self):
-        # Test retrieving a product with a valid product ID
-        response = self.app.get('/get_product/1')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('name', response.json)
+app = Flask(__name__)
+redis_client = redis.Redis(host='redis', port=6379, db=0)
 
-    def test_get_product_invalid_id(self):
-        # Test retrieving a product with an invalid product ID
-        response = self.app.get('/get_product/invalid_id')
-        self.assertEqual(response.status_code, 400)
-        self.assertIn('error', response.json)
+consumer_key = os.getenv('CONSUMER_KEY')
+consumer_secret = os.getenv('CONSUMER_SECRET')
+URL = os.getenv('API_URL')
 
-    def test_get_product_invalid_auth(self):
-        # Test retrieving a product with invalid authentication credentials
-        response = self.app.get('/get_product/1', headers={'Authorization': 'invalid_token'})
-        self.assertEqual(response.status_code, 401)
+@app.route('/add_product', methods=['POST'])
+def add_product():
+    # Get the product data from the request
+    product_data = request.json
 
-    def test_update_product_success(self):
-        # Test updating a product with a valid product ID and valid data
-        product_data = {
-            "name": "Updated Product",
-            "type": "simple",
-            "regular_price": "20.00"
-        }
-        response = self.app.put('/update_product/1', json=product_data)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('message', response.json)
+    # Set up the OAuth1Session for authentication
+    oauth = OAuth1Session(client_key=consumer_key, client_secret=consumer_secret)
 
-    def test_update_product_invalid_id(self):
-        # Test updating a product with an invalid product ID and valid data
-        product_data = {
-            "name": "Updated Product",
-            "type": "simple",
-            "regular_price": "20.00"
-        }
-        response = self.app.put('/update_product/invalid_id', json=product_data)
-        self.assertEqual(response.status_code, 400)
-        self.assertIn('error', response.json)
+    # Set up the API endpoint and headers
+    f'{URL}/delete_product/{product_id}',
+    url = 'http://192.168.10.10:8080/wp-json/wc/v3/products'
+    headers = {'Content-Type': 'application/json'}
 
-    def test_update_product_invalid_data(self):
-        # Test updating a product with a valid product ID and invalid data
-        product_data = {
-            "name": "Updated Product",
-            "type": "simple",
-            "regular_price": "invalid_price"
-        }
-        response = self.app.put('/update_product/1', json=product_data)
-        self.assertEqual(response.status_code, 400)
-        self.assertIn('error', response.json)
+    # Send the POST request to add the product
+    response = oauth.post(url, headers=headers, json=product_data)
 
-    def test_update_product_invalid_auth(self):
-        # Test updating a product with invalid authentication credentials
-        product_data = {
-            "name": "Updated Product",
-            "type": "simple",
-            "regular_price": "20.00"
-        }
-        response = self.app.put('/update_product/1', json=product_data, headers={'Authorization': 'invalid_token'})
-        self.assertEqual(response.status_code, 401)
+    # Handle the response from the WooCommerce API
+    if response.status_code == 201:
+        # Extract the product_id from the response body
+        product_id = response.json()['id']
+        return jsonify({'message': 'Product added successfully.', 'product_id': product_id}), 201
+    else:
+        return jsonify({'error': 'Failed to add product.'}), 400
 
-    def test_delete_product_success(self):
-        # Test deleting a product with a valid product ID
-        response = self.app.delete('/delete_product/1')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('message', response.json)
+@app.route('/delete_product/<product_id>', methods=['DELETE'])
+def delete_product(product_id):
+    # Set up the OAuth1Session for authentication
+    oauth = OAuth1Session(client_key=consumer_key, client_secret=consumer_secret)
 
-    def test_delete_product_invalid_id(self):
-        # Test deleting a product with an invalid product ID
-        response = self.app.delete('/delete_product/invalid_id')
-        self.assertEqual(response.status_code, 400)
-        self.assertIn('error', response.json)
+    # Set up the API endpoint and headers
+    url = f'http://192.168.10.10:8080/wp-json/wc/v3/products/{product_id}'
+    headers = {'Content-Type': 'application/json'}
 
-    def test_delete_product_invalid_auth(self):
-        # Test deleting a product with invalid authentication credentials
-        response = self.app.delete('/delete_product/1', headers={'Authorization': 'invalid_token'})
-        self.assertEqual(response.status_code, 401)
+    # Send the DELETE request to delete the product
+    response = oauth.delete(url, headers=headers)
+
+    # Handle the response from the WooCommerce API
+    if response.status_code == 200:
+        return jsonify({'message': 'Product deleted successfully.'}), 200
+    else:
+        return jsonify({'error': 'Failed to delete product.'}), 400
+
+@app.route('/update_product/<product_id>', methods=['PUT'])
+def update_product(product_id):
+    # Get the product data from the request
+    product_data = request.json
+
+    # Set up the OAuth1Session for authentication
+    oauth = OAuth1Session(client_key=consumer_key, client_secret=consumer_secret)
+
+    # Set up the API endpoint and headers
+    url = f'http://192.168.10.10:8080/wp-json/wc/v3/products/{product_id}'
+    headers = {'Content-Type': 'application/json'}
+
+    # Send the PUT request to update the product
+    response = oauth.put(url, headers=headers, json=product_data)
+
+    # Handle the response from the WooCommerce API
+    if response.status_code == 200:
+        return jsonify({'message': 'Product updated successfully.'}), 200
+    else:
+        return jsonify({'error': 'Failed to update product.'}), 400
+
+@app.route('/get_product/<product_id>', methods=['GET'])
+def get_product(product_id):
+    # Set up the OAuth1Session for authentication
+    oauth = OAuth1Session(client_key=consumer_key, client_secret=consumer_secret)
+
+    # Set up the API endpoint and  headers 
+    url = f'http://192.168.10.10:8080/wp-json/wc/v3/products/{product_id}'
+    headers = {'Content-Type': 'application/json'}
+
+    # Send the GET request to retrieve the product
+    response = oauth.get(url, headers=headers)
+
+    # Handle the response from the WooCommerce API
+    if response.status_code == 200:
+        product = response.json()
+        return jsonify(product), 200
+    else:
+        return jsonify({'error': 'Failed to retrieve product.'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
