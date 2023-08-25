27ef590ce4697385d8a15bd6fc50542223eb61bf
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-08-25 22:25:00 +0200

    updated product.py

--- product.py
+++ product.py
@@ -4,10 +4,10 @@ from requests_oauthlib import OAuth1Session
 import redis
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() 
 
 app = Flask(__name__)
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_client = redis.Redis(host='redis', port=6379, db=0)
 
 consumer_key = os.getenv('CONSUMER_KEY')
 consumer_secret = os.getenv('CONSUMER_SECRET')
