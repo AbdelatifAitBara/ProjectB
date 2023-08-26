85ce9b096e2b6f789bd8b83bb74aecc20861157e
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-08-26 20:19:06 +0200

    edited test_product.py

--- test_product.py
+++ test_product.py
@@ -6,8 +6,8 @@ import json
 class TestAPI(unittest.TestCase):
     def setUp(self):
         self.base_url = 'http://192.168.10.30:8080'
-        self.consumer_key = 'ck_871224cf87f8459b0862453fa7e03dbe2accbecd'
-        self.consumer_secret = 'cs_3f3dc3a74524e7f7442303a6e12ec03de5da505c'
+        self.consumer_key = 'ck_c86d458381c645d1709dbbf95ae3033226871df8'
+        self.consumer_secret = 'cs_a413d091ad0ebc09df06e547dcbb787e0256baeb'
 
     def test_add_product(self):
         # Define the product data to add
