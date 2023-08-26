584f37c8527e92aff7467e780a74e016275d209b
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-08-26 21:49:26 +0200

    added order.py/Jenkinsfile and edited the project structure

--- Jenkinsfile
+++ Jenkinsfile
@@ -9,32 +9,43 @@ pipeline {
       }
     }
 
-    stage('Build Docker Image') {
+    stage('Build Product Image') {
       steps {
-        sh 'docker build -f /home/vagrant/agent/ProjectB/Dockerfile -t product_microservice:$(git rev-parse --short=7 HEAD) /home/vagrant/agent/ProjectB/'
+        sh 'docker build -f /home/vagrant/agent/ProjectB/product/Dockerfile -t product_microservice:$(git rev-parse --short=7 HEAD) /home/vagrant/agent/ProjectB/product/Dockerfile'
       }
     }
 
-    stage('Remove Container Running on port 8080') {
+    stage('Build Order Image') {
+      steps {
+        sh 'docker build -f /home/vagrant/agent/ProjectB/order/Dockerfile -t order_microservice:$(git rev-parse --short=7 HEAD) /home/vagrant/agent/ProjectB/order/Dockerfile'
+      }
+    }
+
+    stage('Remove Containers Running on Ports 8080 and 9090') {
       steps {
         script {
           def containers = sh(
-            script: 'docker ps --filter "publish=8080" -q',
+            script: 'docker ps --filter "publish=8080" --filter "publish=9090" -q',
             returnStdout: true
           ).trim()
           if (containers) {
             sh "docker rm -f ${containers}"
           } else {
-            echo "No containers running on port 8080"
+            echo "No containers running on ports 8080 and 9090"
           }
         }
       }
     }
     
-    stage('Deploy container') {
+    stage('Deploy Product Microservice') {
       steps {
-        sh 'docker rm -f product_microservice_container'
         sh 'docker run -d -p 8080:8080 --name product_microservice_container product_microservice:$(git rev-parse --short=7 HEAD)'
+      }
+    }
+
+    stage('Deploy Order Microservice') {
+      steps {
+        sh 'docker run -d -p 9090:9090 --name order_microservice_container order_microservice:$(git rev-parse --short=7 HEAD)'
       }
     }
 
---
+++ product/product.py
@@ -0,0 +1,96 @@
+import os
+from flask import Flask, jsonify, request
+from requests_oauthlib import OAuth1Session
+import redis
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+redis_client = redis.Redis(host='redis', port=6379, db=0)
+
+consumer_key = os.getenv('CONSUMER_KEY')
+consumer_secret = os.getenv('CONSUMER_SECRET')
+
+@app.route('/add_order', methods=['POST'])
+def add_order():
+    # Get the order data from the request
+    order_data = request.json
+
+    # Set up the OAuth1Session for authentication
+    oauth = OAuth1Session(client_key=consumer_key, client_secret=consumer_secret)
+
+    # Set up the API endpoint and headers
+    url = 'http://192.168.10.10:8080/wp-json/wc/v3/orders'
+    headers = {'Content-Type': 'application/json'}
+
+    # Send the POST request to add the order
+    response = oauth.post(url, headers=headers, json=order_data)
+
+    # Handle the response from the WooCommerce API
+    if response.status_code == 201:
+        order_id = response.json()['id']
+        return jsonify({'message': 'Order added successfully.', 'order_id': order_id}), 201
+    else:
+        return jsonify({'error': 'Failed to add order.'}), 400
+
+@app.route('/delete_order/<order_id>', methods=['DELETE'])
+def delete_order(order_id):
+    # Set up the OAuth1Session for authentication
+    oauth = OAuth1Session(client_key=consumer_key, client_secret=consumer_secret)
+
+    # Set up the API endpoint and headers
+    url = f'http://192.168.10.10:8080/wp-json/wc/v3/orders/{order_id}'
+    headers = {'Content-Type': 'application/json'}
+
+    # Send the DELETE request to delete the order
+    response = oauth.delete(url, headers=headers)
+
+    # Handle the response from the WooCommerce API
+    if response.status_code == 200:
+        return jsonify({'message': 'Order deleted successfully.'}), 200
+    else:
+        return jsonify({'error': 'Failed to delete order.'}), 400
+
+@app.route('/update_order/<order_id>', methods=['PUT'])
+def update_order(order_id):
+    # Get the order data from the request
+    order_data = request.json
+
+    # Set up the OAuth1Session for authentication
+    oauth = OAuth1Session(client_key=consumer_key, client_secret=consumer_secret)
+
+    # Set up the API endpoint and headers
+    url = f'http://192.168.10.10:8080/wp-json/wc/v3/orders/{order_id}'
+    headers = {'Content-Type': 'application/json'}
+
+    # Send the PUT request to update the order
+    response = oauth.put(url, headers=headers, json=order_data)
+
+    # Handle the response from the WooCommerce API
+    if response.status_code == 200:
+        return jsonify({'message': 'Order updated successfully.'}), 200
+    else:
+        return jsonify({'error': 'Failed to update order.'}), 400
+
+@app.route('/get_order/<order_id>', methods=['GET'])
+def get_order(order_id):
+    # Set up the OAuth1Session for authentication
+    oauth = OAuth1Session(client_key=consumer_key, client_secret=consumer_secret)
+
+    # Set up the API endpoint and headers
+    url = f'http://192.168.10.10:8080/wp-json/wc/v3/orders/{order_id}'
+    headers = {'Content-Type': 'application/json'}
+
+    # Send the GET request to retrieve the order
+    response = oauth.get(url, headers=headers)
+
+    # Handle the response from the WooCommerce API
+    if response.status_code == 200:
+        order = response.json()
+        return jsonify(order), 200
+    else:
+        return jsonify({'error': 'Failed to retrieve order.'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
