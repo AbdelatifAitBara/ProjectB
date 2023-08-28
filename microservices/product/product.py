6c7135c1f2c457495f91cb1e2244c54c5c4f5942
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-08-28 22:14:32 +0200

    Edited product.py

--- microservices/product/product.py
+++ microservices/product/product.py
@@ -52,7 +52,7 @@ def get_token():
     )
     
     # Define the query
-    query = "SELECT role FROM product WHERE role = 'shop manager' AND password = %s and username = %s;"
+    query = "SELECT role FROM public.product WHERE role = 'shop manager' AND password = %s and username = %s;"
 
     # Create a cursor to execute the query
     cur = conn.cursor()
