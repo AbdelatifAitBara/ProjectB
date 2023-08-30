ea1de7de0bb124d497d7da38d4cbb0e5755f9213
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-08-30 20:52:45 +0200

    Improve product.py SEC

--- microservices/product/product.py
+++ microservices/product/product.py
@@ -4,7 +4,7 @@ import os
 import jwt
 from datetime import datetime, timedelta
 from functools import wraps
-import pymysql
+import mysqlx
 
 app = Flask(__name__)
 
@@ -36,25 +36,27 @@ def token_required(f):
     return decorated
 
 def check_credentials(username, password):
-    # Connect to the MySQL database using PyMySQL
-    connection = pymysql.connect(host='192.168.10.10',
-                                 user='root',
-                                 password='password',
-                                 db='wordpress_db',
-                                 port=3306,
-                                 cursorclass=pymysql.cursors.DictCursor)
+    # Connect to the MySQL database using X protocol
+    session = mysqlx.get_session({
+        'host': '192.168.10.10',
+        'port': 33060,
+        'user': 'root',
+        'password': 'password'
+    })
 
-    with connection.cursor() as cursor:
-        # Check if the username, password, and role are correct
-        query = f"SELECT u.ID, u.user_login, u.user_email, m.meta_value FROM wp_users u JOIN wp_usermeta m ON u.ID = m.user_id WHERE m.meta_key = 'wp_capabilities' AND u.user_login='{username}' AND u.user_pass='{password}' AND m.meta_value LIKE '%shop_manager%'"
-        cursor.execute(query)
-        result = cursor.fetchone()
+    schema = session.get_schema('wordpress_db')
+
+    # Check if the username, password, and role are correct
+    result = schema.get_table('wp_users').select().where(
+        "user_login = :username and user_pass = :password and exists(select * from wp_usermeta where user_id = wp_users.ID and meta_key = 'wp_capabilities' and meta_value like '%shop_manager%')").bind(
+        'username', username).bind('password', password).execute().fetch_one()
 
     # Close the database connection
-    connection.close()
+    session.close()
 
     # Return True if the credentials are correct, otherwise False
     return result is not None
+
 
 @app.route('/token', methods=['POST'])
 def get_token():
