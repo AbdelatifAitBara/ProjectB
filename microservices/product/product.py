7361824db8e8893f942ff6fd79b9a0cb74063f4c
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-08-31 11:35:49 +0200

    Product.py fields SEC

--- microservices/product/product.py
+++ microservices/product/product.py
@@ -8,40 +8,26 @@ consumer_key = os.getenv('CONSUMER_KEY')
 consumer_secret = os.getenv('CONSUMER_SECRET')
 api_url = os.getenv('API_URL')
 
-# Function to validate the product data
-def validate_product_data(product_data):
-    if not product_data:
-        abort(400, 'Product data is required.')
-
-    # Check for empty fields
-    if not all(product_data.values()):
-        abort(400, 'Empty fields are not allowed.')
-
-    # Check for special characters
-    for key, value in product_data.items():
-        if isinstance(value, str) and not value.isalnum() and key != 'regular_price':
-            abort(400, f'Special characters are not allowed in {key}.')
-
-    # Check for excessively long input
-    max_length = 100  # Set the maximum length as per your requirement
-    for key, value in product_data.items():
-        if isinstance(value, str) and len(value) > max_length:
-            abort(400, f'{key} exceeds the maximum length of {max_length} characters.')
-
-    # Check for regular_price as number
-    if 'regular_price' in product_data:
-        try:
-            float(product_data['regular_price'])
-        except ValueError:
-            abort(400, 'Regular price must be a number.')
-
 @app.route('/add_product', methods=['POST'])
 def add_product():
     # Get the product data from the request
     product_data = request.json
 
-    # Validate the product data
-    validate_product_data(product_data)
+    # Check for empty fields
+    required_fields = ['name', 'regular_price', 'short_description', 'description', 'images']
+    for field in required_fields:
+        if field not in product_data or not product_data[field]:
+            abort(400, f"Missing or empty field: {field}")
+
+    # Validate regular_price
+    if not isinstance(product_data['regular_price'], (int, float)):
+        abort(400, "regular_price must be a number")
+
+    # Check for suspicious input
+    suspicious_chars = ['<', '>', ';', "'", '"', '(', ')', '{', '}', '[', ']', '|', '&', '$', '#', '%', '@', '!', '`']
+    for field in product_data:
+        if any(char in product_data[field] for char in suspicious_chars):
+            abort(400, "Input contains suspicious characters")
 
     # Set up the OAuth1Session for authentication
     oauth = OAuth1Session(client_key=consumer_key, client_secret=consumer_secret)
@@ -64,9 +50,106 @@ def add_product():
     else:
         abort(response.status_code, response.text)
 
-# Other route functions...
+@app.route('/delete_product/<product_id>', methods=['DELETE'])
+def delete_product(product_id):
+    # Set up the OAuth1Session for authentication
+    oauth = OAuth1Session(client_key=consumer_key, client_secret=consumer_secret)
 
-# Error handlers...
+    # Set up the API endpoint and headers
+    headers = {'Content-Type': 'application/json'}
+
+    # Send the DELETE request to delete the product
+    try:
+        response = oauth.delete(f'{api_url}/{product_id}', headers=headers)
+        response.raise_for_status()
+    except Exception as e:
+        abort(400, str(e))
+
+    # Handle the response from the WooCommerce API
+    if response.status_code == 200:
+        return jsonify({'message': 'Product deleted successfully.'}), 200
+    else:
+        abort(response.status_code, response.text)
+
+@app.route('/update_product/<product_id>', methods=['PUT'])
+def update_product(product_id):
+    # Get the product data from the request
+    product_data = request.json
+
+    # Set up the OAuth1Session for authentication
+    oauth = OAuth1Session(client_key=consumer_key, client_secret=consumer_secret)
+
+    # Set up the API endpoint and headers
+    headers = {'Content-Type': 'application/json'}
+
+    # Send the PUT request to update the product
+    try:
+        response = oauth.put(f'{api_url}/{product_id}', headers=headers, json=product_data)
+        response.raise_for_status()
+    except Exception as e:
+        abort(400, str(e))
+
+    # Handle the response from the WooCommerce API
+    if response.status_code == 200:
+        return jsonify({'message': 'Product updated successfully.'}), 200
+    else:
+        abort(response.status_code, response.text)
+        
+@app.route('/get_products', methods=['GET'])
+def get_products():
+    # Set up the OAuth1Session for authentication
+    oauth = OAuth1Session(client_key=consumer_key, client_secret=consumer_secret)
+
+    # Set up the API endpoint and headers
+    headers = {'Content-Type': 'application/json'}
+
+    # Send the GET request to retrieve all products
+    try:
+        response = oauth.get(api_url, headers=headers)
+        response.raise_for_status()
+    except Exception as e:
+        abort(400, str(e))
+
+    # Handle the response from the WooCommerce API
+    if response.status_code == 200:
+        products = response.json()
+        return jsonify(products), 200
+    else:
+        abort(response.status_code, response.text)
+
+@app.route('/get_product/<product_id>', methods=['GET'])
+def get_product(product_id):
+    # Set up the OAuth1Session for authentication
+    oauth = OAuth1Session(client_key=consumer_key, client_secret=consumer_secret)
+
+    # Set up the API endpoint and  headers 
+    headers = {'Content-Type': 'application/json'}
+    
+    # Send the GET request to retrieve the product
+    try:
+        response = oauth.get(f'{api_url}/{product_id}', headers=headers)
+        response.raise_for_status()
+    except Exception as e:
+        abort(400, str(e))
+
+    # Handle the response from the WooCommerce API
+    if response.status_code == 200:
+        product = response.json()
+        return jsonify(product), 200
+    else:
+        abort(response.status_code, response.text)
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({'error': str(error)}), 400
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'error': 'Resource not found.'}), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({'error': 'Internal server error.'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
