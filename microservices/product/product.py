cf97a93082ee5d3737e5a81e1183c022b90fd080
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-08-31 10:49:23 +0200

    PRODUCT.PY FIXED

--- microservices/product/product.py
+++ microservices/product/product.py
@@ -89,4 +89,4 @@ def get_product(product_id):
         return jsonify({'error': 'Failed to retrieve product.'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=9090)
+    app.run(debug=True, host='0.0.0.0', port=8080)
