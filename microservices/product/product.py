7527727bcf5c0a3b0cdb387bef4a16cd9503f414
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-09-01 17:07:22 +0200

    Improve Product.py SEC

--- microservices/product/product.py
+++ microservices/product/product.py
@@ -11,10 +11,10 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_DATABASE_USER')
-app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_DATABASE_PASSWORD')
-app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE_DB')
-app.config['MYSQL_DATABASE_HOST'] = os.getenv('MYSQL_DATABASE_HOST')
+app.config['MYSQL_DATABASE_USER'] = os.getenv('DB_HOST')
+app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('DB_PASSWORD')
+app.config['MYSQL_DATABASE_DB'] = os.getenv('DB_NAME')
+app.config['MYSQL_DATABASE_HOST'] = os.getenv('DB_HOST')
 
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
