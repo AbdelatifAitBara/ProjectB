b77d7949a79a166741837376a26468051de8ddfe
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-09-10 09:41:05 +0200

    Restore version1

--- microservices/products/product.py
+++ microservices/products/product.py
@@ -15,9 +15,9 @@ CORS(app)
 app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_DATABASE_USER')
 app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_DATABASE_PASSWORD')
 app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE_DB')
-app.config['MYSQL_DATABASE_HOST'] = "MySQL_Container"
+app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 API_URL = os.getenv('API_URL')
 consumer_key = os.getenv('CONSUMER_KEY')
